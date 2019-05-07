2a27d92930dec0143884225e28ad21781f28b797
Hart Ripley <hart.ripley@arctiq.ca>
2019-05-07 18:13:17 -0400

    remove default facility

--- vars.tf
+++ vars.tf
@@ -9,7 +9,6 @@ variable "packet_project_id" {
 
 variable "packet_facility" {
   description = "Packet facility: US East(ewr1), US West(sjc1), Tokyo (nrt1) or EU(ams1). Default: ewr1"
-  default = "ewr1"
   type = "list"
 }
 
